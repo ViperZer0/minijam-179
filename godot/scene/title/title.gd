3bfe7f980d9936ea6544ff7a981313797b2d4721
ViperZer0 <11594471+ViperZer0@users.noreply.github.com>
2025-02-28 22:57:44 -0800

    Added quit button overlay to title scene

--- godot/scene/title/title.gd
+++ godot/scene/title/title.gd
@@ -3,8 +3,18 @@ extends MarginContainer
 @export_file("*.tscn") var main_scene_path: String = ""
 @onready var main_scene: Control = load(main_scene_path).instantiate()
 @onready var scene_transition_service: SceneTransitionService = ServiceProvider.get_service("SceneTransitionService")
+@onready var quit_overlay: CanvasLayer = %QuitOverlay
+
+func _ready() -> void:
+	# Hide the quit button overlay on the web.
+	# I think you can close browser games? but idk, maybe that doesn't make much sense
+	if OS.get_name() == "Web":
+		quit_overlay.hide()
 
 func _on_sound_button_pressed():
 	scene_transition_service.zoom_in(self, main_scene)
+
+func _on_quit_button_pressed():
+	get_tree().quit()
 
 
