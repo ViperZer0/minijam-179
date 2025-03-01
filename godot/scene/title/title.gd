eea8f2b2524eed228003603dce1bea75801f2366
ViperZer0 <11594471+ViperZer0@users.noreply.github.com>
2025-02-28 23:00:06 -0800

    Fixed bug with canvas layer not being zoomed in

--- godot/scene/title/title.gd
+++ godot/scene/title/title.gd
@@ -3,13 +3,13 @@ extends MarginContainer
 @export_file("*.tscn") var main_scene_path: String = ""
 @onready var main_scene: Control = load(main_scene_path).instantiate()
 @onready var scene_transition_service: SceneTransitionService = ServiceProvider.get_service("SceneTransitionService")
-@onready var quit_overlay: CanvasLayer = %QuitOverlay
+@onready var quit_button: SoundButton = %QuitButton
 
 func _ready() -> void:
 	# Hide the quit button overlay on the web.
 	# I think you can close browser games? but idk, maybe that doesn't make much sense
 	if OS.get_name() == "Web":
-		quit_overlay.hide()
+		quit_button.hide()
 
 func _on_sound_button_pressed():
 	scene_transition_service.zoom_in(self, main_scene)
