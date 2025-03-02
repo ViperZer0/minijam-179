d33a02ad1b1bf72be0390f4e5bd45b692859bf8c
ViperZer0 <11594471+ViperZer0@users.noreply.github.com>
2025-03-02 13:58:46 -0800

    Added a silly goofy emitter that follows your mouse cursor :3

---
+++ godot/component/background/background.gd
@@ -0,0 +1,19 @@
+extends ColorRect
+@export var emitter_speed: float = 800.0
+
+@onready var particle_emitter: CPUParticles2D = %ParticleEmitter
+@onready var timer: Timer = $ParticleBurstCooldown
+
+func _process(_delta: float) -> void:
+	var velocity = get_local_mouse_position() - particle_emitter.position
+	velocity = velocity.normalized() * emitter_speed * _delta
+	particle_emitter.position += velocity
+
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("trigger_particle_burst"):
+		print("Boo!")
+		particle_emitter.initial_velocity_max = 1000
+		timer.start()
+
+func _on_particle_burst_cooldown_timeout():
+	particle_emitter.initial_velocity_max = 100
